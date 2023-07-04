e9e00aa5bcb7d304098743915b20d47c8affbf35
gurjaspreet <107759543+gurjaspreet@users.noreply.github.com>
2023-07-04 18:11:58 +0530

    Day 3 completed :)

---
+++ Day 3/Exercise1.py
@@ -0,0 +1,5 @@
+A = (2, 4)
+B = (-4, 6)
+
+mid = ((A[0] + B[0]) / 2, (A[1] + B[1]) / 2)
+print(f'The middle point: {mid}')
---
+++ Day 3/Exercise2.py
@@ -0,0 +1,5 @@
+A = (3, 2)
+B = (-1, -1)
+
+dist = ((A[0] - B[0]) ** 2 + (A[1] - B[1]) ** 2) ** 0.5
+print(f'The distance between points A and B: {dist:.1f}')

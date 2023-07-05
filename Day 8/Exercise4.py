c3472cb76e57df4bff37036a3abe8b99907bf16f
gurjaspreet <107759543+gurjaspreet@users.noreply.github.com>
2023-07-05 20:00:05 +0530

    Day 8 completed :)

---
+++ Day 8/Exercise4.py
@@ -0,0 +1,8 @@
+url = (
+    'https://e-smartdata.teachable.com/'
+    'p/sciezka-data-scientist-machine-learning-engineer'
+)
+
+name = url.split('/')[-1]
+name = name.replace('-', ' ')
+print(name)

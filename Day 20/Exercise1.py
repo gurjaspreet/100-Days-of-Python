15a3fa3ee6c626ebb30ddb3bb3391dddb57ba627
gurjaspreet <107759543+gurjaspreet@users.noreply.github.com>
2023-07-11 17:52:35 +0530

    Day 20 completed :)

---
+++ Day 20/Exercise1.py
@@ -0,0 +1,11 @@
+project_ids = {
+    '01': 'open', 
+    '02': 'new',
+    '03': 'in progress',
+    '04': 'completed'
+}
+
+if project_ids.get('02') == 'new':
+    project_ids['02'] = 'open'
+
+print(project_ids)
---
+++ Day 20/Exercise2.py
@@ -0,0 +1,7 @@
+item = '001'
+items = ['001', '000', '003', '005', '006']
+
+if item in items:
+    items.remove(item)
+    
+print(items)
